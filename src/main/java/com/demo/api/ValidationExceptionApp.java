01994b66b6785716b4488fcad55d1079f31cf145
get2gpreet <get2gpreet@gmail.com>
2024-09-24 15:05:00 -0400

    First commit

---
+++ src/main/java/com/demo/api/ValidationExceptionApp.java
@@ -0,0 +1,13 @@
+package com.demo.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationExceptionApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationExceptionApp.class, args);
+	}
+
+}
---
+++ src/main/java/com/demo/api/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.demo.api.repository;
+
+import com.demo.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository  extends JpaRepository<User,Integer> {
+    User findByUserId(int id);
+}
